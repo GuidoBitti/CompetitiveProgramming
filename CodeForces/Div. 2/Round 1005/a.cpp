5abacf923323a423f79848a949bf7b348eb29d04
Guido Bitti <guidobitti@gmail.com>
2025-02-22 12:57:00 -0500

    div 1005-virtual

---
+++ CodeForces/Div. 2/Round 1005/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+// g++ -std=c++20 -02 -wall template.cpp -o template
+ 
+#define forr(i,a,b) for(int i=(int)a ; i<(int)b ; i++)
+#define forn(i, n) for(int i = 0; i < n; i ++)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define all(x) (x).begin(),(x).end()
+#define fst first
+#define snd second
+#define pb(x) push_back(x)
+#define X real()
+#define Y imag()
+#define MAXN 200000
+#define MAXI 9e18
+#define MINI -9e18
+typedef long long ll;
+typedef complex<ll> P;
+typedef pair<int,int> ii;
+ 
+int main(){
+    cout.tie(0);
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--){
+        char ini='0';
+        string bs;
+        int n,sol=0; 
+        cin >> n>>bs;
+        forn(i,n){
+            if(bs[i]!=ini){
+                ini=bs[i];
+                sol++;
+            }
+        }
+        cout << sol << '\n';
+    }
+    
+    return 0;
+}
